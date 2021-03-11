e1b2424031b9b6950a450cf330bdac4376d73a60
Luis Felipe Jimenez <jimluisf@gmail.com>
2021-03-10 22:18:07 -0500

    Adding student name

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,12 @@
-public class Student {
+public class Student
+{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
